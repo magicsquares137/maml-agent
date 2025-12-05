1170c10e2d5ae2d8cca129f4ce5e8716355cbfaf
magicsquares137 <magicsquares137@gmail.com>
2025-12-04 22:22:10 -0500

    edit

--- baseline/templates.py
+++ baseline/templates.py
@@ -151,29 +151,32 @@ from dataclasses import dataclass
 @dataclass 
 class Template:
     template: str = """USER:
-    I am your supervisor and you are a super intelligent AI Assistant whose job is to
-    achieve my day-to-day tasks completely autonomously.
+    I am your supervisor and you are a super intelligent AI Assistant whose job is to achieve my day-to-day tasks completely autonomously.
 
-    To do this, you will need to interact with app/s (e.g., spotify, venmo etc) using
-    their associated APIs on my behalf. For this you will undertake a *multi-step
-    conversation* using a python REPL environment.
+    To do this, you will need to interact with app/s (e.g., spotify, venmo etc) using their associated APIs on my behalf. 
+    For this you will undertake a *multi-step conversation* using a python REPL environment. That is, you will write the 
+    python code and the environment will execute it and show you the result, based on which, you will write python code 
+    for the next step and so on, until you've achieved the goal. This environment will let you interact with app/s using their associated APIs on my behalf.
 
-    Here are three key APIs that you need to know to get more information:
-    ```python
+    Here are three key APIs that you need to know to get more information
+
+    # To get a list of apps that are available to you.
     print(apis.api_docs.show_app_descriptions())
+
+    # To get the list of apis under any app listed above, e.g. spotify
     print(apis.api_docs.show_api_descriptions(app_name='spotify'))
+
+    # To get the specification of a particular api, e.g. spotify app's login api
     print(apis.api_docs.show_api_doc(app_name='spotify', api_name='login'))
-    ```
 
-    Each code execution will produce an output that you can use in subsequent calls.
+    Each code execution will produce an output that you can use in subsequent calls. Using these APIs, you can now 
+    generate code, that I will execute, to solve the task. Let's start with the task
 
-    Using these APIs, you can now generate code, that I will execute, to solve the task.
-
-    My name is: {first_name} {last_name}. My personal email is {email} and phone number is {phone_number}.
+    My name is: Jane Doe. My personal email is jane@example.com and phone number is 555-1234.
 
     Task: How many playlists do I have in Spotify?
 
-    A:
+    ASSISTANT:
     Okay. Lets first find which APIs are available to use in Spotify.
     Code:
     ```python
@@ -186,11 +189,12 @@ class Template:
     [
     ...
     "login : Login to your account.",
+    "logout : Logout from your account.",
     ...
     ]
     ```
 
-    A:
+    ASSISTANT:
     Okay. Looks like I can use the `login` api. Lets find its specifications.
     Code:
     ```python
@@ -201,14 +205,14 @@ class Template:
     Output:
     ```
     {{
-      "app_name": "spotify",
-      "api_name": "login",
-      "parameters": [...],
-      "response_schema": {{"access_token": "string"}}
+    "app_name": "spotify",
+    "api_name": "login",
+    "parameters": [...],
+    "response_schema": {{"access_token": "string"}}
     }}
     ```
 
-    A:
+    ASSISTANT:
     Okay, I'll need the password. Let me get that from the supervisor.
     Code:
     ```python
@@ -219,9 +223,9 @@ class Template:
             spotify_password = account["password"]
             break
 
-    response = apis.spotify.login(username="{email}", password=spotify_password)
+    response = apis.spotify.login(username="jane@example.com", password=spotify_password)
     spotify_access_token = response["access_token"]
-    print(f"Logged in successfully.")
+    print("Logged in successfully.")
     ```
 
     USER:
@@ -230,56 +234,77 @@ class Template:
     Logged in successfully.
     ```
 
-    A:
-    The `show_playlist_library` API seems to be the one we need.
+    ASSISTANT:
+    The `show_playlist_library` API seems to be the one we need. Let's check its specifications.
+    Code:
+    ```python
+    print(apis.api_docs.show_api_doc(app_name='spotify', api_name='show_playlist_library'))
+    ```
+
+    USER:
+    Output:
+    ```
+    {{
+    "app_name": "spotify",
+    "api_name": "show_playlist_library",
+    "parameters": [
+      {{"name": "access_token", "type": "string", "required": true}},
+      {{"name": "page_index", "type": "integer", "required": false, "default": 0}}
+    ],
+    "response_schema": [
+      {{"playlist_id": 1, "title": "string", "song_ids": [1]}}
+    ]
+    }}
+    ```
+
+    ASSISTANT:
+    I need to page through all the playlists to get the list of playlists and save it in `playlists`.
     Code:
     ```python
     page_index = 0
     playlists = []
     while page_index < 10:
-        playlist_page = apis.spotify.show_playlist_library(
-            access_token=spotify_access_token, 
-            page_index=page_index
-        )
+        playlist_page = apis.spotify.show_playlist_library(access_token=spotify_access_token, page_index=page_index)
         if playlist_page:
             playlists.extend(playlist_page)
             page_index += 1
         else:
             break
     num_playlists = len(playlists)
-    print(f"Total playlists: {{num_playlists}}")
+    print(num_playlists)
     ```
 
     USER:
     Output:
     ```
-    Total playlists: 23
+    23
     ```
 
-    A:
-    Now that I have the answer, I'll complete the task with the answer.
+    ASSISTANT:
+    Now that the task is completed, I can call apis.supervisor.complete_task(). Since this task has an answer to be returned, I will pass that as an argument.
     Code:
     ```python
     apis.supervisor.complete_task(answer=num_playlists)
-    ``` 
+    ```
 
     USER:
     Output:
     Marked the active task complete.
 
-    -----------------------------------------------------------
+    ----------------------------------------------
 
     USER:
     **Key instructions**:
-    (1) End code blocks with ``` followed by a newline.
-    (2) You can reuse variables from previous code blocks.
-    (3) Email addresses and tokens in the example above are not valid anymore.
-    (4) Use "supervisor" app for account info and "phone" app for contacts.
-    (5) Always check API specs with apis.api_docs.show_api_doc before calling.
-    (6) Write small chunks of code, one per step.
-    (7) Many APIs return "pages". Loop through all pages with `page_index`.
-    (8) CRITICAL: When the task asks for information, you MUST pass it as answer=<value> to complete_task(). If the task asks you to DO something (not return information), call complete_task() with NO answer parameter.
-    (9) CRITICAL: Before calling complete_task(), verify that you have actually made the required changes. Do not complete the task prematurely.
+    (1) Make sure to end code blocks with ``` followed by a newline.
+    (2) Remember you can use the variables in your code in subsequent code blocks.
+    (3) Remember that the email addresses, access tokens and variables (e.g. spotify_password) in the example above are not valid anymore.
+    (4) You can use the "supervisor" app to get information about my accounts and use the "phone" app to get information about friends and family.
+    (5) Always look at API specifications (using apis.api_docs.show_api_doc) before calling an API.
+    (6) Write small chunks of code and only one chunk of code in every step. Make sure everything is working correctly before making any irreversible change.
+    (7) Many APIs return items in "pages". Make sure to run through all the pages by looping over `page_index`.
+    (8) Once you have completed the task, make sure to call apis.supervisor.complete_task(). If the task asked for some information, 
+        return it as the answer argument, i.e. call apis.supervisor.complete_task(answer=<answer>). Many tasks do not require an answer, 
+        so in those cases, just call apis.supervisor.complete_task() i.e. do not pass any argument.
 
     USER:
     Using these APIs, now generate code to solve the actual task:
